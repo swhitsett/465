2e37e08770f5dd1248167d8b55f3c6892e82a098
swhitsett <kongzero32@gmail.com>
2014-10-03 06:04:06 +0000

    need to get comparson

--- wordladder/app/controllers/home_controller.rb
+++ wordladder/app/controllers/home_controller.rb
@@ -1,19 +1,23 @@
 class HomeController < ApplicationController
-
+  load "#{Rails.root}/lib/ladder.rb"
+ 
   def index
     # don't need to do anything here
   end
 
   def show
-    @query_text = Array.new
-   # @query_text = [:query,:query2,:query3,:query5]
+      @query_text = Array.new
       @query_text.push(params[:query])
       @query_text.push(params[:query2])
       @query_text.push(params[:query3])
       @query_text.push(params[:query4])
       @query_text.push(params[:query5])
-
-      @result = @query_text # @query_text #`whois #{@query_text}`.split("\n)
+     
+      if(edit_distance_of_one "dog","dog" == true)	 
+      	@result = @query_text # @query_text #`whois #{@query_text}`.split("\n)
+      else
+	@result = "damit"
+      end
   end
 
 end
