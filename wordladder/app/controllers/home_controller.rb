30b883e26b8399ade94c8394b3cfe7a3eb8a8418
swhitsett <kongzero32@gmail.com>
2014-10-04 18:39:51 -0700

    fixingcontroler

--- wordladder/app/controllers/home_controller.rb
+++ wordladder/app/controllers/home_controller.rb
@@ -6,19 +6,26 @@ class HomeController < ApplicationController
   end
 
   def show
-      @query_text = Array.new
-      @query_text.push(params[:start_word])
-      @query_text.push(params[:query])
-      @query_text.push(params[:query2])
-      @query_text.push(params[:query3])
-      @query_text.push(params[:query4])
-      @query_text.push(params[:query5])
-     
-    if(edit_distance_of_one "dog","dog")	 
-      	@result = @query_text # @query_text #`whois #{@query_text}`.split("\n)
+    @query_text = Array.new
+    @query_text.push(params[:start_word])
+    @query_text.push(params[:query1]) if params[:query1] != ""
+    @query_text.push(params[:query2]) if params[:query2] != ""
+    @query_text.push(params[:query3]) if params[:query3] != ""
+    @query_text.push(params[:query4]) if params[:query4] != ""
+    @query_text.push(params[:query5]) if params[:query5] != ""
+    @query_text.push(params[:end_word])
+    # @query_text.compact!
+
+    @query_text.each do {|check|
+      if(edit_distance_of_one (check, @query_text.at(@query_text.index(check)+1)))
+
       else
-	@result = "damit"
+
       end
+    }
+    
+
+
   end
 
 end
