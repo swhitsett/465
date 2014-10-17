477b51890f32b16fd694b0f6dd73375694806b3b
swhitsett <kongzero32@gmail.com>
2014-10-16 21:24:30 -0700

    patch error

--- scorebook/app/controllers/high_scores_controller.rb
+++ scorebook/app/controllers/high_scores_controller.rb
@@ -10,4 +10,36 @@ class HighScoresController < ApplicationController
       @high_score = HighScore.find(params[:id])
    end
 
+  # GET /high_scores/new
+  def new
+    @high_score = HighScore.new
+  end
+
+  # POST /high_scores
+  def create
+   @high_score = HighScore.new(params.require(:high_score).permit(:user, :game, :score))
+    if @high_score.save
+      redirect_to @high_score, notice: 'High score was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /high_scores/1/edit
+  def edit
+    @high_score = HighScore.find(params[:id])
+  end
+
+
+  # PATCH /high_scores/1
+  def update
+    @high_score = HighScore.find(params[:id])
+    if @high_score.update(params.require(:high_score).permit(:user, :game, :score))
+      redirect_to @high_score, notice: 'High score was successfully updated.'  
+    else
+      render :edit
+    end
+  end
+
 end
+
