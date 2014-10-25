cc2d559afe23650904e3cb90b12f190385db3e6b
swhitsett <kongzero32@gmail.com>
2014-10-25 01:36:08 -0700

    seeds created

--- doi_server/db/seeds.rb
+++ doi_server/db/seeds.rb
@@ -1,14 +1,22 @@
 dois = Doi.create(
   [
-    { name: 'Sally', desc: 'Jones', url: 'Chico State'},
-    { name: 'Fred', desc: 'Smith', url: 'Chico State'}
+    { name: 'tacos', desc: 'delicious/bad', url: 'md89j3pg8h5lb7ghs,48h6yh5m,g7sdh'},
+    { name: 'School', desc: 'future drinking problems', url: 'testdoi1'},
+    { name: 'yarp', desc: 'phrase from movie', url: 'testdoi2'},
+    { name: 'This', desc: 'Was', url: 'testdoi3'},
+    { name: 'Tired', desc: 'its 2:00am', url: 'testdoi4'},
+    { name: 'Snow', desc: 'I wanna go snowboarding', url: '86753098675309'}
   ]
 )
 
 urls = Url.create(
   [
-    { site: 'PHYS 204B', rating: 4, doi_id: dois[0].id},
-    { site: 'PHYS 204B', rating: 78, doi_id: dois[0].id},
-    { site: 'MATH 217', rating: 0, doi_id: dois[1].id}
+    { site: 'www.tacobell.com', doi_id: dois[0].id},
+    { site: 'www.ChicoState.com', doi_id: dois[1].id},
+    { site: 'www.ChicoState.com/sportsNstuff', doi_id: dois[1].id},
+    { site: 'http://www.hotfuzz.com', doi_id: dois[2].id},
+    { site: 'Hard.edu', doi_id: dois[3].id},
+    { site: 'poop.com', doi_id: dois[4].id},
+    { site: 'www.mountshasta.com', doi_id: dois[5].id}
   ]
 )
