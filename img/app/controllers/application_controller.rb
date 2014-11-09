debcdf030ebf597346df619739855f0eb7ddcf35
swhitsett <kongzero32@gmail.com>
2014-11-09 13:13:53 -0800

    toLaptop

--- img/app/controllers/application_controller.rb
+++ img/app/controllers/application_controller.rb
@@ -4,11 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
   
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
       protected
           def configure_permitted_parameters
               devise_parameter_sanitizer.for(:sign_up) << :name
               devise_parameter_sanitizer.for(:account_update) << :name
           end
+
 end
